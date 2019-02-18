6cd8039f692724b741be63d5c40f976829b23893
Martin <martin810907@gmail.com>
2019-02-18 08:26:00 +0100

    Update userscripts.js
    Changed to show the Compendiums menu.

--- code/userscripts.js
+++ code/userscripts.js
@@ -1,44 +1,88 @@
-/* Links to be shown in the floating menu */
-var links = [
-	'<a href="https://www.dndbeyond.com">HOME</a>',
-	'<a href="https://www.dndbeyond.com/search">SEARCH</a>',
-	'',
-	'RULES',
-	'<a href="https://www.dndbeyond.com/compendium/rules/phb">PHB</a>',
-	'<a href="https://www.dndbeyond.com/compendium/rules/dmg">DMG</a>',
-	'<a href="https://www.dndbeyond.com/compendium/rules/mm">MM</a>',
-	'<a href="https://www.dndbeyond.com/compendium/rules/scag">SCAG</a>',
-	'<a href="https://www.dndbeyond.com/compendium/rules/xgte">XGtE</a>',
-	'',
-	'ADVENTURES',
-	'<a href="https://www.dndbeyond.com/compendium/adventures/lmop">LMoP</a>',
-	'<a href="https://www.dndbeyond.com/compendium/adventures/hotdq">HotDQ</a>',
+/* Create the floating menu div*/
+var myFloatingMenu = document.createElement("DIV");
+myFloatingMenu.className = "myFloatingMenu";
 
-	//DON'T TOUCH THE ROW BELOW, IT'S USED TO TOGGLE THE USER BUTTONS
-	"<a style=\"font-size:0.75em;\" onclick=\"if(document.getElementsByClassName('site-interactions-group-user')[0].style.display != 'block'){document.getElementsByClassName('site-interactions-group-user')[0].style.display ='block'} else {document.getElementsByClassName('site-interactions-group-user')[0].style.display ='none'}\"><br/>Show User Menu</a>"
-];
+/* Check if ToC is created */
+if (sessionStorage.my_menu) {
+	myFloatingMenu.innerHTML = sessionStorage.my_menu;
+} else {
+	sessionStorage.my_menu = "";
+	sessionStorage.menuTocList = "false";
 
+	var toc = "";
+	/* Create 'View introduction' menu element */
+	var intro = document.getElementsByClassName("compendium-toc-blockquote")[0].getElementsByTagName("a")[1];
+	sessionStorage.my_menu = '<div class="myIntroButton"><a style="color:#47D18C;" href="' + intro.href + '">' + intro.innerText + '</a></div>';
 
+	/* Copy ToC from document. There's two different types of ToCs:
+	compendium-toc-list as used on PHB and DMG etc
+	compendium-toc-full-text as used on MM, LMoP, XGtE etc*/
+	toc = document.getElementsByClassName("compendium-toc-list")[0];
+	
+	if (typeof toc === "undefined" || toc.length == 0) {
+		sessionStorage.menuTocList = "false";
+		toc = document.getElementsByClassName("compendium-toc-full-text")[0];
+		/* Number of child items and how many columns are used: 
+		MM = 57 => 5
+		Basic = 39 => 4
+		HotDQ = 20 => 3
+		XGtE = 5 => 3
+		LMoP = 15 => 3
+		*/
+		if (toc.childElementCount < 25)
+			sessionStorage.numColumns = "3";
+		else if (toc.childElementCount < 45) 
+			sessionStorage.numColumns = "4";
+		else	
+			sessionStorage.numColumns = "5";
 
-/* Insert link to Font used in floating menu*/
-var fontLink = document.createElement("LINK");
-fontLink.rel = "stylesheet";
-fontLink.href = "https://fonts.googleapis.com/css?family=Fredericka+the+Great";
-document.getElementsByTagName("head")[0].appendChild(fontLink);
+	} else {
+		sessionStorage.menuTocList = "true";
+	}
 
-/* Create the floating menu and insert into document */
-var electronFloatingMenu = document.createElement("DIV");
-electronFloatingMenu.className = "electron-floating-menu";
-electronFloatingMenu.innerHTML = links.join("<br/>");
-
-document.body.appendChild(electronFloatingMenu);
-
-electronFloatingMenu.onmouseenter = function(){document.getElementsByClassName("electron-floating-menu")[0].style.opacity = "1.0";};
-electronFloatingMenu.onmouseleave = function(){document.getElementsByClassName("electron-floating-menu")[0].style.opacity = "0.0";};
-
-
-/* Restore top row (logo, search box etc) and menus when not in a compendium */
-if (document.URL.indexOf("compendium") == -1) {
-	document.getElementsByClassName("site-bar")[0].style.display = "block"; /* Top row */
-	document.getElementsByTagName("nav")[0].style.display = "block"; /* Menus */
+	sessionStorage.my_menu += toc.innerHTML;
+	
+	myFloatingMenu.innerHTML = sessionStorage.my_menu;
 }
+
+/* The two different ToCs use different CSS */
+if (sessionStorage.menuTocList == "true") {
+	myFloatingMenu.style.display = "none";
+	myFloatingMenu.style.flexWrap = "wrap";
+	myFloatingMenu.style.columnCount = "auto";
+} else {
+	myFloatingMenu.style.display = "none";
+	myFloatingMenu.style.flex = "1 1 0%";
+	myFloatingMenu.style.columnCount = sessionStorage.numColumns;
+}
+
+/* Add the floating menu to the document and hide/show with the M key */
+document.body.appendChild(myFloatingMenu);
+document.addEventListener("keypress", showMenuFunction);
+
+function showMenuFunction(myKeyEvent){
+	if (myKeyEvent.key == "m" || myKeyEvent.key == "M") {
+		var display = document.getElementsByClassName("myFloatingMenu")[0].style.display;
+		if (display == "none") {
+			if (sessionStorage.menuTocList == "true") 
+				document.getElementsByClassName("myFloatingMenu")[0].style.display = "flex";
+			else
+				document.getElementsByClassName("myFloatingMenu")[0].style.display = "inline";
+		} else
+			document.getElementsByClassName("myFloatingMenu")[0].style.display = "none";
+	}
+}
+
+/* Change number of columns when window is smaller */
+function changeColumnsFunction(x) {
+	if (sessionStorage.menuTocList == "false")
+		if (x.matches) { // If media query matches
+			myFloatingMenu.style.columnCount = sessionStorage.numColumns;
+		} else {
+			myFloatingMenu.style.columnCount = sessionStorage.numColumns - 1;
+		}
+  }
+  
+  var x = window.matchMedia("(min-width: 800px)")
+  changeColumnsFunction(x)
+  x.addListener(changeColumnsFunction)
