3fba03f542a09097fe3f6fb45aeae80faf60431d
Martin <martin810907@gmail.com>
2019-03-04 11:05:35 +0100

    Create character_sheet.js

---
+++ code/character_sheet.js
@@ -0,0 +1,22 @@
+function widthMediaQuery(x) {
+	if (x.matches) { // If media query matches
+		document.getElementsByClassName("body-rpgcharacter-sheet")[0].style.backgroundPosition = "center 116px";
+	} else if (window.matchMedia("(min-width: 1024px)").matches) {
+		document.getElementsByClassName("body-rpgcharacter-sheet")[0].style.backgroundPosition = "center 94px";
+	} else if (window.matchMedia("(min-width: 768px)").matches) {
+		document.getElementsByClassName("body-rpgcharacter-sheet")[0].style.backgroundPosition = "center 94px";
+		window.setTimeout(function() {
+			document.getElementById("site-main").style.paddingTop = "0px";
+		}, 2000);
+	} else if (window.matchMedia("(max-width: 767px)").matches) {
+		window.setTimeout(function() {
+			alert("hej");
+			document.getElementsByClassName("ct-character-sheet-mobile__header")[0].style.top = "0px";
+			document.getElementById("site-main").style.paddingTop = "0px";
+		}, 2000);	
+	}
+}
+	
+var x = window.matchMedia("(min-width: 1200px)");
+widthMediaQuery(x);
+x.addListener(widthMediaQuery);
